941ad7790d1cea4ef3e0e251d73624fb9761e76e
ramagammer <skwgrama@gmail.com>
2025-03-20 23:53:29 -0300

    New inicio

--- front/src/components/Hero.jsx
+++ front/src/components/Hero.jsx
@@ -19,10 +19,10 @@ const Hero = ({ scrollToServicios }) => {
       <div className="absolute inset-0 bg-[#222222] opacity-80"></div>
       <div className="z-10">
         <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl text-[#F5F1EB] font-semibold tracking-wide">
-          Escribania
+          Escribania - La Riva
         </h1>
         <h2 className="text-lg sm:text-xl md:text-3xl lg:text-4xl text-[#D1AE85] tracking-wider mt-2 sm:mt-4">
-          La Riva
+        Seguridad Juridica
         </h2>
         <button
           onClick={scrollToServicios}
