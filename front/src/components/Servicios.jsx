3edc0a5bdee57106bc47135bc0c4bd9414daef3d
ramagammer <skwgrama@gmail.com>
2025-03-22 23:48:48 -0300

    Changes

--- front/src/components/Servicios.jsx
+++ front/src/components/Servicios.jsx
@@ -7,20 +7,20 @@ const Servicios = ({ image, title, description, id }) => {
 
   return (
     <div 
-      className="bg-[#A89E97] ml-4 rounded-2xl shadow-md hover:shadow-lg hover:-translate-y-2 transition-transform duration-500 max-w-[300px] w-full h-[450px] flex flex-col"
+      className="bg-[#A89E97] ml-4 rounded-2xl shadow-md hover:shadow-lg hover:-translate-y-2 transition-transform duration-500 max-w-[280px] w-full h-[400px] flex flex-col"
       style={{
-        maxWidth: window.innerWidth < 768 ? "250px" : "300px",
-        height: window.innerWidth < 768 ? "400px" : "450px"
+        maxWidth: window.innerWidth < 768 ? "230px" : "280px",
+        height: window.innerWidth < 768 ? "370px" : "400px"
       }}
     >
-      <img src={image} alt={title} className="w-full h-44 object-cover rounded-t-2xl" />
+      <img src={image} alt={title} className="w-full h-40 object-cover rounded-t-2xl" />
 
-      <div className="p-6 flex flex-col flex-grow justify-between items-center text-center">
-        <h2 className="text-[#F5F1EB] text-xl font-bold mb-2">{title}</h2>
-        <p className="text-[#F5F1EB] text-sm flex-grow">{description}</p>
+      <div className="p-4 flex flex-col flex-grow justify-between items-center text-center">
+        <h2 className="text-[#F5F1EB] text-lg font-semibold mb-2">{title}</h2>
+        <p className="text-[#F5F1EB] text-xs flex-grow">{description}</p>
         <Link 
           to={`/servicios/${id}`} 
-          className="bg-[#D1AE85] text-white py-2 px-4 rounded-lg font-medium shadow-md hover:bg-[#A67C52] transition mt-4"
+          className="bg-[#D1AE85] text-white py-2 px-3 rounded-lg font-medium shadow-md hover:bg-[#A67C52] transition mt-3 text-sm"
         >
           {t("servicios.agendar_consulta")}
         </Link>
